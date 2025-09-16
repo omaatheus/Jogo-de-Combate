40a81a8e433ee1191989f7256cd501068b2014c7
Matheuss <matheusgida@outlook.com>
2025-09-16 18:25:23 -0300

    Mecânica de Combate

--- jogo.py
+++ jogo.py
@@ -22,6 +22,17 @@ class Personagem:
     def exibirDetalhes(self):
         return f"\nNome: {self.getNome()} \nVida: {self.getVida()} \nNivel: {self.getNivel()}"
 
+    def receberAtaque(self, dano):
+        self.__vida -= dano
+        if self.__vida < 0:
+            self.__vida = 0
+
+    def atacar(self, alvo):
+        dano = self.__nivel * 2
+        alvo.receberAtaque(dano)
+        print(f"{self.getNome()} atacou {alvo.getNome()} e causou {dano} de dano!")
+
+
 
 class Heroi(Personagem):
     def __init__(self, nome, vida, nivel, habilidade):
@@ -33,6 +44,11 @@ class Heroi(Personagem):
 
     def exibirDetalhes(self):
         return f"{super().exibirDetalhes()}\nHabilidade: {self.getHabilidade()}"
+
+    def ataqueEspecial(self, alvo):
+        dano = self.getNivel() * 5
+        alvo.receberAtaque(dano)
+        print(f"\n{self.getNome()} atacou com seu poder de {self.getHabilidade()}!")
 
 class Inimigo(Personagem):
     def __init__(self, nome, vida, nivel, tipo):
@@ -48,7 +64,7 @@ class Inimigo(Personagem):
 
 class Jogo:
     """Classe que orquestra o Jogo"""
-    def __init__(self, ):
+    def __init__(self):
         self.heroi = Heroi("Rodolfo", 100, 5, "Força")
         self.inimigo = Inimigo("Luzas", 50, 3, "Ave")
 
@@ -63,6 +79,21 @@ class Jogo:
                 input("|======Pressione <Enter> para atacar... ======|")
                 escolha = input("\n|====== Escolha de ataque ======|\n1 - Ataque Normal\n2 - Ataque Especial\n> ")
 
+                if escolha == "1":
+                    self.heroi.atacar(self.inimigo)
+                elif escolha == "2":
+                    self.heroi.ataqueEspecial(self.inimigo)
+                else:
+                    print("\n|====== Escolha Inválida, escolha novamente ======|")
+
+                if self.inimigo.getVida() > 0:
+                    self.inimigo.atacar(self.heroi)
+
+
+            if self.heroi.getVida() > 0:
+                print(f"\n|====== Parabéns, você ganhou a batalha! ======|")
+            else:
+                print(f"\n|====== Infelizmente, você perdeu a batalha! ======|")
 
 
 jogo = Jogo()
